a8de463a3910fed19e55aac2838e249db57e4396
Raisa Ramírez <raisa.ramirezreyes@gmail.com>
2025-03-31 21:32:09 -0600

    Fixing bug

--- interfaces/index.tsx
+++ interfaces/index.tsx
@@ -2,7 +2,7 @@ import { Dispatch, SetStateAction } from "react";
 
 export interface AppContextProps {
     pokemon: any;
-    setPokemon: Dispatch<SetStateAction<PokemonProps>>;
+    setPokemon: Dispatch<SetStateAction<string>>;
 }
 
 export interface PokemonProps {
